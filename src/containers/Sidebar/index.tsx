90cac9d153e900535c9a4a39527c89f337a71237
Júlio Cézar <jc.amorim.n01@gmail.com>
2024-06-25 21:45:30 -0300

    construindo o layout sobre

---
+++ src/containers/Sidebar/index.tsx
@@ -0,0 +1,10 @@
+import Titulo from '../../components/Titulo'
+
+const Sidebar = () => (
+  <aside>
+    <img src="https://github.com/jcamorim.png" />
+    <Titulo>Júlio Cézar</Titulo>
+  </aside>
+)
+
+export default Sidebar
---
+++ src/containers/Sobre/index.tsx
@@ -0,0 +1,14 @@
+import Titulo from '../../components/Titulo'
+
+const Sobre = () => (
+  <section>
+    <Titulo>Sobre mim</Titulo>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesentium,
+      quas rerum sinteos voluptatum dicta architecto quia labore, veniam
+      quisquam quos, quod officiis debitis facilis beatae modi? Eaque, venian
+    </p>
+  </section>
+)
+
+export default Sobre
